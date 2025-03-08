af54455c7b0ed6bcf13669f17226de24c1d39c8f
Kotoba <48874632+KotobaGit@users.noreply.github.com>
2025-03-08 21:29:30 +1100

    Camera Follow and Clamps Added

---
+++ CISSA Game Jam/Assets/CameraFollow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 camVelocity = Vector3.zero;
+    public float smoothTime;
+    [SerializeField] private Transform robot;
+
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    void Start()
+    {
+        // Camera Clamp Ranges
+        yMin = 0f;
+        yMax = 0f;
+        xMin = 0f;
+        xMax = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xClamp = Mathf.Clamp(robot.position.x, xMin, xMax); // The player's x value, but within the clamp range. IE gives the player's position, until its outside the clamp range
+        float yClamp = Mathf.Clamp(robot.position.y, yMin, yMax); // The player's y value, but within the clamp range. IE gives the player's position, until its outside the clamp range
+
+        Vector3 clampedpos = new Vector3(xClamp, yClamp, -1); // turns the clamp into a vector3
+        Vector3 smoothpos = Vector3.SmoothDamp(transform.position, clampedpos, ref camVelocity, smoothTime * Time.deltaTime); // gradually smooths the target position, which is the clamped player position
+        transform.position = smoothpos; // moves the camera to the target position
+    }
+}
