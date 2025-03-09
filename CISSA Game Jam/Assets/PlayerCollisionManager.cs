c925431a965c5d831f2985527497c1a16390ebcd
Kotoba <48874632+KotobaGit@users.noreply.github.com>
2025-03-10 00:03:45 +1100

    Generator Room Programming + Minor Tweaks

--- CISSA Game Jam/Assets/PlayerCollisionManager.cs
+++ CISSA Game Jam/Assets/PlayerCollisionManager.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 public class PlayerCollisionManager : MonoBehaviour
 {
     private int buttonsPressed = 0;
-    public GameObject door;
+    private int buttonsInRoom = 4; // number of buttons in the room
+    public GameObject barrier;
     private SpriteRenderer sr;
     private PlayerHealthManager playerHealthManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,7 +17,10 @@ public class PlayerCollisionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (buttonsPressed == buttonsInRoom)
+        {
+            Destroy(barrier);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision) // use this for handling collisions with objects that have a Collider2D (No Trigger)
     {
@@ -30,15 +34,32 @@ public class PlayerCollisionManager : MonoBehaviour
             case "Button": // If the trigger the player collides with is tagged "Button", will call the HandleButtonCollision function
                 sr = collision.GetComponent<SpriteRenderer>();
                 sr.color = Color.green; // Sets the button's color to green
-                Destroy(door); // Removes all door GameObjects. Can be altered to perform an animation (open the door) etc. Have different gameobjects for each door in the world?
+                buttonsPressed += 1;
+                collision.tag = "Untagged"; // Removes the buttons tag, so it cant call this again
                 break;
 
             case "ElectricWater":
                 playerHealthManager.playerTakeDamage(20); // Robot takes 20 damage when it collides with Electric Water
                 break;
 
-            case "SpawnRoomDoor":
+            case "DoorToRecRoom":
                 SceneManager.LoadScene("Rec Room");
+                break;
+
+            case "DoorToSpawnRoom":
+                SceneManager.LoadScene("Spawn Room");
+                break;
+
+            case "DoorToGeneratorRoom":
+                SceneManager.LoadScene("Generator Room");
+                break;
+
+            case "DoorToCockpitRoom":
+                SceneManager.LoadScene("Cockpit Room");
+                break;
+
+            case "DoorToAirlockRoom":
+                SceneManager.LoadScene("Airlock Room");
                 break;
 
         }
