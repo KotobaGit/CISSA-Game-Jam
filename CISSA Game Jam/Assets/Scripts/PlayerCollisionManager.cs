cd3ed25547ea924194d5457bfee8fba7a3d83ad4
Kotoba <48874632+KotobaGit@users.noreply.github.com>
2025-03-10 13:22:40 +1100

    Major Scene /  Room Development

---
+++ CISSA Game Jam/Assets/Scripts/PlayerCollisionManager.cs
@@ -0,0 +1,118 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerCollisionManager : MonoBehaviour
+{
+    private int buttonsPressed = 0;
+    private int buttonsInRoom = 4; // number of buttons in the generator room
+    public GameObject generatorBarrier1;
+    public GameObject generatorBarrier2;
+    private SpriteRenderer sr;
+    public Image healthBar;
+    private float playerHealth = 100f; // Players health
+
+    private bool canLeaveRoom = false;
+    private bool foundAutopilotPassword = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonsPressed == buttonsInRoom) // Destroys the 2 barriers in the generator room when all buttons are pressed
+        {
+            Destroy(generatorBarrier1);
+            Destroy(generatorBarrier2);
+        }
+        if (playerHealth <1) // Reloads the current scene if playerhealth falls below 1
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) // use this for handling collisions with objects that have a Collider2D (No Trigger)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // use this for handling collision with objects that have a Collider2D (Trigger)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Button": // If the trigger the player collides with is tagged "Button", will call the HandleButtonCollision function
+                sr = collision.GetComponent<SpriteRenderer>();
+                sr.color = Color.green; // Sets the button's color to green
+                buttonsPressed += 1;
+                collision.tag = "Untagged"; // Removes the buttons tag, so it cant call this again
+                break;
+
+            case "AutopilotPassword": // When the player finds the autopilot password note, sets foundAutopilotPassword to true. Add dialogue here
+                foundAutopilotPassword = true;
+                break;
+
+            case "GenComputer":
+                canLeaveRoom = true;  // Player can interact with the door to the rec room. Add checkmark to journal here
+                break;
+
+            case "AutopilotComputer":
+                if (foundAutopilotPassword == true)
+                {
+                    canLeaveRoom = true; // Player can leave the room if they interact with the autopilot computer after they have found the password on the note. Add checkmark to journal here
+                }
+                break;
+
+            case "ElectricWater":
+                Debug.Log("HIT WATER");
+                PlayerTakeDamage(20); // Robot takes 20 damage when it collides with Electric Water
+                break;
+
+            case "DoorToRecRoom":
+                if (canLeaveRoom == true)
+                {
+                    SceneManager.LoadScene("Rec Room");
+                }
+                break;
+
+            case "DoorToSpawnRoom":
+                {
+                    SceneManager.LoadScene("Spawn Room");
+                }
+                break;
+
+            case "DoorToGeneratorRoom":
+                {
+                    SceneManager.LoadScene("Generator Room");
+                }
+                break;
+
+            case "DoorToCockpitRoom":
+                {
+                    SceneManager.LoadScene("Cockpit Room");
+                }
+                break;
+
+            case "DoorToAirlockRoom":
+                {
+                    SceneManager.LoadScene("Airlock Room");
+                }
+                break;
+
+            case "SuctionPoint":
+                {
+                    PlayerTakeDamage(100);
+                }
+                break;
+
+        }
+    }
+
+    public void PlayerTakeDamage(float damage) // Use this function to make the player take a certain amount of damage
+    {
+        playerHealth -= damage;
+        healthBar.fillAmount = playerHealth / 100f;
+    }
+}
