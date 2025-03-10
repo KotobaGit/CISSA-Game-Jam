de1931671f962b0c1cf700ce7051249ef30bc08d
Kotoba <48874632+KotobaGit@users.noreply.github.com>
2025-03-10 20:17:53 +1100

    Small Update

--- CISSA Game Jam/Assets/DialogueManagerScript.cs
+++ CISSA Game Jam/Assets/DialogueManagerScript.cs
@@ -2,20 +2,34 @@ using UnityEngine;
 
 public class DialogueManagerScript : MonoBehaviour
 {
+    // Place GameObjects holding dialogue here
     public GameObject introTextObject; // GameObject holding the Intro Text dialogue
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Place other GameObjects here
+    public GameObject robot;
+    public GameObject dialogueBox;
+
     void Start()
     {
         if (introTextObject == null) // Finds the Intro Text if its null at start
         {
             introTextObject = GameObject.Find("Intro Text");
             introTextObject.GetComponent<TextBoxObject>().SendText();
+            
+        }
+        if (robot == null)
+        {
+            robot = GameObject.FindGameObjectWithTag("Robot"); // Finds the Robot tag if null
+        }
+        if (dialogueBox == null)
+        {
+            dialogueBox = GameObject.Find("")
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
