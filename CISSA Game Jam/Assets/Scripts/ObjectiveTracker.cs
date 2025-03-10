f4cf063d4f6bcacfeb80f0c36bc7a3f171cdbedb
Wontkins <zane.sekkouah@gmail.com>
2025-03-10 17:27:14 +1100

    objective tracker basics

--- CISSA Game Jam/Assets/Scripts/ObjectiveTracker.cs
+++ CISSA Game Jam/Assets/Scripts/ObjectiveTracker.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class ObjectiveTracker : MonoBehaviour
 {
+    private int ObjectivesAmount = 3;
+    [SerializeField] public static bool[] Objective = new bool[3 - 1]; //number of objectives -1
+    [SerializeField] private GameObject[] ObjectiveTicks;
+    [SerializeField] private GameObject HiddenObjective;
+    [SerializeField] private GameObject HiddenObjectiveTick;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
-        
+        for (int i = 0; i < ObjectivesAmount; i++) //at the start of every scene
+        {
+            TickObjective(i, Objective[i]);
+        }
+    }
+    public void TickObjective(int num,bool ticked)
+    {
+        Objective[num] = ticked;
+        ObjectiveTicks[num].SetActive(ticked);
+        for (int i = 0; i < ObjectivesAmount; i++)
+        {
+            if (!Objective[i])
+            {
+                HiddenObjective.SetActive(false);
+                HiddenObjectiveTick.SetActive(false);
+                return;
+            }
+            //if it passes the for loop then we can start the last hidden objective
+        }
+        HiddenObjective.SetActive(true);
     }
 
     // Update is called once per frame
-    void Update()
+    public void TickHiddenObjective()
     {
-        
+        HiddenObjectiveTick.SetActive(true);
     }
 }
